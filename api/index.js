ea8282f1c1875a23ffe9b6505b95a5924a6bab6a
dkoh2018 <105094753+dkoh2018@users.noreply.github.com>
2024-05-31 01:30:55 -0500

    final joke

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.redirect("https://www.illinois.edu/");
+  res.redirect("https://www.uchicago.edu/");
 });
 
 app.listen(8000, () => {
